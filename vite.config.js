ece7fbd14d8e0349e439fdccfb2d6ee7e287e986
shivam89954 <shivampal9310@gmail.com>
2026-05-17 00:12:05 +0530

    updated project

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,16 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
     tsconfigPaths({ projects: ["./tsconfig.json"] }),
     tailwindcss(),
     tanstackStart(),
+    nitro({
+      preset: "vercel",
+    }),
     react(),
   ],
   resolve: {
